2357f9e0a1274cfbdf55e7b2fd0a846e005b1553
chaitanya-jadhav11 <chaitanyabj@gmail.com>
2026-04-18 22:35:40 +0530

    env set-up

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+
+
+def main():
+    print("Hello from ai-agents-with-langchain-and-langgraph!")
+
+if __name__ == "__main__":
+    main()
